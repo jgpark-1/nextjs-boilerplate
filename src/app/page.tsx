9e25b1f3042801a85815b457a1ef73f5f62996f1
jgpark-1 <jgpark7281@gmail.com>
2023-11-10 03:40:36 +0900

    feat: add main start intro

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,43 @@
+"use client";
+
+import { Fragment, useState } from "react";
+
 import Link from "next/link";
 import Image from "next/image";
 
 import clsx from "clsx";
 
-import styles from "./page.module.scss";
+import Background from "./Background";
+
+import styles from './page.module.scss'
 
 export default function Home() {
+  const [isTriggered, setIsTriggered] = useState(false);
+
+  const onStartClick = () => {
+    setIsTriggered(true);
+  };
+
   return (
-    <main className="w-screen h-screen">
-      <article></article>
-    </main>
+    <Fragment>
+      <section className="w-full h-full">
+        <article className="w-full h-full">
+          <div className="w-full h-full">
+            <div className="relative z-10 flex items-center justify-center w-full h-full">
+              <div className="w-full h-full mx-auto max-w-7xl">
+                <div className="flex items-center justify-center w-full h-full">
+                  {!isTriggered && (
+                    <button className={styles.button} onClick={onStartClick} type="button">
+                      start
+                    </button>
+                  )}
+                </div>
+              </div>
+            </div>
+          </div>
+        </article>
+      </section>
+      <Background isTriggered={isTriggered} />
+    </Fragment>
   );
 }
