d6405d376efad071fde7d24f73a4b842e966a084
jgpark-1 <jgpark7281@gmail.com>
2023-11-11 03:14:28 +0900

    feat: add ScrollDown component

---
+++ src/components/arrows/ScrollDown.tsx
@@ -0,0 +1,19 @@
+import clsx from "clsx";
+
+import styles from "./ScrollDown.module.scss";
+
+export default function ScrollDown() {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.circle1} />
+      <div className={clsx(styles.circle1, styles.circle2)} />
+      <div className={styles.arrow}>
+        <div className={styles.arrowLine} />
+        <div className={styles.arrowTipWrapper}>
+          <div className={clsx(styles.arrowTip, styles.left)} />
+          <div className={clsx(styles.arrowTip, styles.right)} />
+        </div>
+      </div>
+    </div>
+  );
+}
